fb2257d17cf4b617a30d181cb3f5f8fc5b9e329a
MarkKowalski <mark.kowalski@op.pl>
2016-09-22 20:03:13 +0100

    initial

---
+++ Controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+include('models/view.php');
+class IndexController extends View{
+    public $request = null;
+    public $layout = array();
+    
+    
+    public function __construct(){
+        
+    }
+    public function index(){
+        return $this->view->render('index');
+              
+        
+    }
+}
